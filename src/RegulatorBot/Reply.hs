cdb0175d82705ae2eb5888a29e3af90920a01ce4
lambdajon <lambdajon42@gmail.com>
2025-10-03 01:29:27 +0500

    feat: implement reply message to groups action

--- src/RegulatorBot/Reply.hs
+++ src/RegulatorBot/Reply.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 module RegulatorBot.Reply where
 
-import Data.Text
+import Control.Concurrent.STM (readTVarIO)
+import Control.Monad.IO.Class (liftIO)
 import Data.Text qualified as Text
-import GHC.Stack (HasCallStack)
+import RegulatorBot.State (BotState (..))
+import RegulatorBot.Types (Community (..))
 import Telegram.Bot.API
 import Telegram.Bot.Simple
 
@@ -24,4 +29,24 @@ replyStart = do
   replyMsg =
     (toReplyMessage replyMsgTxt)
       { replyMessageReplyMarkup = Just $ SomeInlineKeyboardMarkup keyboard
+      }
+
+replyCommunities :: BotState -> BotM ()
+replyCommunities model = do
+  c <- liftIO $ readTVarIO model.communities
+  reply $ replyMsg c
+ where
+  mkButton cm = urlButton cm.name cm.github -- Change github url to, chat. But chat is nullable need discussion on what to show.
+  keyboard c =
+    InlineKeyboardMarkup
+      { inlineKeyboardMarkupInlineKeyboard =
+          [mkButton <$> c]
+      }
+  replyMsgTxt =
+    Text.unlines
+      [ "Floss hamjamiyatiga doir barcha guruhlar shu yerda joylashgan" -- TODO: Need correct reply text content, use multiline strings
+      ]
+  replyMsg c =
+    (toReplyMessage replyMsgTxt)
+      { replyMessageReplyMarkup = Just $ SomeInlineKeyboardMarkup (keyboard c)
       }

05fc0fd01024903b5b59c42ad0d25c41657667f5
lambdajon <lambdajon42@gmail.com>
2025-10-03 11:29:15 +0500

    feat: implement /groups command parser and handler

--- src/RegulatorBot/Handler.hs
+++ src/RegulatorBot/Handler.hs
@@ -1,12 +1,16 @@
 module RegulatorBot.Handler where
 
+import GHC.Stack (HasCallStack)
 import RegulatorBot.Reply
 import RegulatorBot.State
 import RegulatorBot.Types
-import GHC.Stack (HasCallStack)
 import Telegram.Bot.Simple.Eff
 
 handleAction :: (HasCallStack) => Action -> Model -> Eff Action Model
 handleAction Start model =
   model <# do
     replyStart
+handleAction Group model =
+  model <# do
+    replyCommunities model
+
--- src/RegulatorBot/Parse.hs
+++ src/RegulatorBot/Parse.hs
@@ -10,11 +10,14 @@ import RegulatorBot.Types
 updateToAction :: Settings -> Update -> Maybe Action
 updateToAction settings@Settings{..} update
   | isCommand "start" update = handleStart settings update
+  | isCommand "group" update = handleGroup settings update
   | otherwise = Nothing
  where
   isCommand cmd = isJust . parseUpdate (commandWithBotName botName cmd)
 
 handleStart :: Settings -> Update -> Maybe Action
-handleStart settings Update{..} = Just Start
+handleStart _ Update{..} = Just Start
 
 
+handleGroup :: Settings -> Update -> Maybe Action
+handleGroup _ Update{..} = Just Group 
